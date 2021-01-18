19b4508b292bfbbc02d0db5bc56bb09f00178e75
abdarum <kornelstefanczyk@wp.pl>
2021-01-18 18:04:20 +0200

    Added multi windows fxml application basement

--- Source_code/javafxFrontend/src/App.java
+++ Source_code/javafxFrontend/src/App.java
@@ -1,35 +1,29 @@
+import java.io.Console;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import login.LoginManager;
 
 public class App extends Application {
-    // public static void main(String[] args) throws Exception {
-    // System.out.println("Hello, World!");
-    // }
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+    public void start(Stage stage) {
+        Scene scene = new Scene(new StackPane());
+        
+        LoginManager loginManager = new LoginManager(scene);
+        loginManager.showLoginScreen();
+    
+        stage.setScene(scene);
+        stage.show();
     }
 }
